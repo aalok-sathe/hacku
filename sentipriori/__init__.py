f538584b7dd6de756d5ff1f3282544d47002abad
aalok.sathe <aalok.sathe@gmail.com>
2019-03-21 16:32:36 -0400

    testing and debuggingh

--- sentipriori/__init__.py
+++ sentipriori/__init__.py
@@ -12,21 +12,38 @@ import sentiment
 class SentiPrioriProc:
     texts = None
     issues = None
+    nlp = sentiment.NLPBackend()
 
 
     def __init__(self):
-        texts = []
+        self.texts = []
 
 
     def reset(self):
         self.texts = []
 
 
-senti = SentiPrioriProc()
+    def add_text(self, txt):
+        txtbdy = text.TextBody(txt, self.nlp)
+        self.texts.append(txtbdy)
 
 
-def process_text(raw_text):
-    texts += [raw_text]
+    def get_texts(self):
+        return iter(self.texts)
+
+_senti = SentiPrioriProc()
+
+def add_text(*args, **kwargs):
+    _senti.add_text(*args, **kwargs)
+
+# def process_text(raw_text):
+#     texts += [raw_text]
+
+add_text('hello darkness my old friend')
+it = _senti.get_texts()
+tb = [*it][0]
+print(tb)
+print(tb.getsentiment())
 
 
 if __name__ == '__main__':
