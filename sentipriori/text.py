7ee9755a9d5ba3045ebef096c2ece3588862e042
aalok.sathe <aalok.sathe@gmail.com>
2019-03-21 16:32:36 -0400

    testing and debuggingh

--- sentipriori/text.py
+++ sentipriori/text.py
@@ -14,19 +14,32 @@ class TextBody:
     sentences = None
     tokens = None
     # sentiments = None
-    nlp = None
+    backend = None
     trees = None
 
 
-    def __init__(self, text=None):
-        self.nlp = sentiment.NLPBackend()
+    def __init__(self, text=None, backend=None):
+        # self.nlp = sentiment.NLPBackend()
         self.text = text
-        self.sentences = nlp.annotate(text)['sentences']
+        self.sentences = backend.annotate(text)['sentences']
         # spans = [*PunktSentenceTokenizer().span_tokenize(text_acc)]
         # self.sentences = [text[a:b] for a, b in spans]
         # self.tokens = [token for token in text.split(' ')]
 
-        self.trees = [Tree.fromtring(s) for s in self.sentences]
-        self.labels = [[subtree.label(), subtree.flatten()]
-                       for subtree in self.trees.subtrees()]
-        self.sentiments = None
+        self.trees = [Tree.fromstring(s['sentimentTree'])
+                      for s in self.sentences]
+        self.labels = [[(subtree.label(), ' '.join(subtree.flatten()))
+                       for subtree in tree.subtrees()] for tree in self.trees]
+        self.sentiments = [[sentiment.getsentimentfromlabel(label)
+                           for label, _ in sentlabels]
+                           for sentlabels in self.labels]
+
+    # def positives(self):
+    #     return [[flat for (sent, prob), (label, flat) in zip(sentiments, labels) if sent >= 3 and prob > .2] for sentiments, labels in self.sentiments, self.labels]
+    #
+    # def negatives(self):
+    #     return [[flat for (sent, prob), (label, flat) in zip(sentiments, labels) if sent <= 1 and prob > .2] for sentiments, labels in self.sentiments, self.labels]
+
+
+    def getsentiment(self):
+        return [sentiment.overallsentiment(t) for t in self.trees]
