fa8003c3d15757cb12055230b4266d843067dc70
aalok.sathe <aalok.sathe@gmail.com>
2019-03-21 12:17:54 -0400

    initial commit of some files

---
+++ text.py
@@ -0,0 +1,17 @@
+
+
+
+class TextBody:
+    text = None
+    sentences = None
+    tokens_list = None
+    sentiments = None
+
+    def __init__(self, text=None):
+        self.text = text
+
+
+
+class Email(TextBody):
+    def __init__(self, )
+        super()
